9035168cc6cfc764a559bb68f871eab64bd7ffb0
botamochi6277 <botamochi6277@icloud.com>
2022-09-20 00:56:54 +0900

    add taskmanager

--- include/tail_util.hpp
+++ include/tail_util.hpp
@@ -15,154 +15,139 @@
 
 #include <Arduino.h>
 #include <Servo.h>
-#include "signal.hpp"
+
 #include "LSM6DS3.h"
+#include "signal.hpp"
 
-enum DIRECTION
-{
-    UP,
-    DOWN,
-    RIGHT,
-    LEFT
-};
+enum DIRECTION { UP, DOWN, RIGHT, LEFT };
 
-void initServo(Servo &servo, int pin)
-{
-    servo.attach(pin, 600, 2400);
-    servo.write(90); // rotate to initial orientation
+void initServo(Servo &servo, int pin) {
+  servo.attach(pin, 600, 2400);
+  servo.write(90);  // rotate to initial orientation
 }
 
-class Tail
-{
-private:
-    Servo servo_h_;
-    Servo servo_v_;
-    int angle_h_;
-    int angle_v_;
-    int amp_h_ = 60;
-    int amp_v_ = 0;
-    int duration_ = 5000; // msec
-    int elapsed_time_ = 0;
-    bool is_swingwing_ = false;
+class Tail {
+ private:
+  Servo servo_h_;
+  Servo servo_v_;
+  int angle_h_;
+  int angle_v_;
+  int amp_h_ = 60;
+  int amp_v_ = 0;
+  int duration_ = 5000;  // msec
+  int elapsed_time_ = 0;
+  bool is_swingwing_ = false;
 
-public:
-    Tail(/* args */);
-    ~Tail();
-    void attachPin(int pin_h, int pin_v);
-    void beginSwing(int amp_h, int amp_v, int duration, int ease = 0);
-    void beginRandomSwing();
-    void update(int step);
-    bool inline isSwinging() { return this->is_swingwing_; }
-    int inline angleH() { return this->angle_h_; }
-    int inline angleV() { return this->angle_v_; }
+ public:
+  Tail(/* args */);
+  ~Tail();
+  void attachPin(int pin_h, int pin_v);
+  void beginSwing(int amp_h, int amp_v, int duration, int ease = 0);
+  void beginRandomSwing();
+  void update(int step);
+  bool inline isSwinging() { return this->is_swingwing_; }
+  int inline angleH() { return this->angle_h_; }
+  int inline angleV() { return this->angle_v_; }
 };
 
 Tail::Tail(/* args */) {}
 
 Tail::~Tail() {}
 
-void Tail::beginSwing(int amp_h, int amp_v, int duration, int ease)
-{
-    this->amp_h_ = amp_h;
-    this->amp_v_ = amp_v;
-    this->duration_ = duration;
+void Tail::beginSwing(int amp_h, int amp_v, int duration, int ease) {
+  this->amp_h_ = amp_h;
+  this->amp_v_ = amp_v;
+  this->duration_ = duration;
+  this->elapsed_time_ = 0;
+  this->is_swingwing_ = true;
+}
+
+void Tail::beginRandomSwing() {
+  int m = random(60, 80);
+  float angle = random(0, 360) * PI / 180.0f;
+  int h = m * cos(angle);
+  int v = m * sin(angle);
+
+  int t = random(2000, 3000);
+  this->beginSwing(h, v, t);
+}
+
+void Tail::update(int step) {
+  if (this->is_swingwing_) {
+    this->elapsed_time_ += step;
+    this->angle_h_ = 90 + TrianglePulse(elapsed_time_, int(0.5 * duration_),
+                                        this->amp_h_, duration_);
+    this->angle_v_ = 90 + TrianglePulse(elapsed_time_, int(0.5 * duration_),
+                                        this->amp_v_, duration_);
+    this->servo_h_.write(this->angle_h_);
+    this->servo_v_.write(this->angle_v_);
+  }
+
+  if ((this->elapsed_time_ > this->duration_) && this->is_swingwing_) {
+    Serial.print(this->elapsed_time_);
+    Serial.println(" : finish swinging");
+    this->servo_h_.write(90);
+    this->servo_v_.write(90);
+    this->is_swingwing_ = false;
     this->elapsed_time_ = 0;
-    this->is_swingwing_ = true;
+  }
 }
 
-void Tail::beginRandomSwing()
-{
-    int m = random(60, 80);
-    float angle = random(0, 360) * PI / 180.0f;
-    int h = m * cos(angle);
-    int v = m * sin(angle);
-
-    int t = random(2000, 3000);
-    this->beginSwing(h, v, t);
+void Tail::attachPin(int pin_h, int pin_v) {
+  initServo(this->servo_h_, pin_h);
+  initServo(this->servo_v_, pin_v);
 }
 
-void Tail::update(int step)
-{
-    if (this->is_swingwing_)
-    {
-        this->elapsed_time_ += step;
-        this->angle_h_ = 90 + TrianglePulse(elapsed_time_, int(0.5 * duration_), this->amp_h_, duration_);
-        this->angle_v_ = 90 + TrianglePulse(elapsed_time_, int(0.5 * duration_), this->amp_v_, duration_);
-        this->servo_h_.write(this->angle_h_);
-        this->servo_v_.write(this->angle_v_);
-    }
+class MyImu {
+ private:
+  LSM6DS3 device_;
+  float acc_x_;
+  float acc_y_;
+  float acc_z_;
+  float gyro_x_;
+  float gyro_y_;
+  float gyro_z_;
 
-    if ((this->elapsed_time_ > this->duration_) && this->is_swingwing_)
-    {
-        Serial.print(this->elapsed_time_);
-        Serial.println(" : finish swinging");
-        this->servo_h_.write(90);
-        this->servo_v_.write(90);
-        this->is_swingwing_ = false;
-        this->elapsed_time_ = 0;
-    }
-}
+ public:
+  MyImu(uint8_t bus_type, uint8_t address);
+  inline float accX() { return acc_x_; }
+  inline float accY() { return acc_y_; }
+  inline float accZ() { return acc_z_; }
+  inline float gyroX() { return gyro_x_; }
+  inline float gyroY() { return gyro_y_; }
+  inline float gyroZ() { return gyro_z_; }
+  inline float accSqrt() {
+    return acc_x_ * acc_x_ + acc_y_ * acc_y_ + acc_z_ * acc_z_;
+  }
 
-void Tail::attachPin(int pin_h, int pin_v)
-{
-    initServo(this->servo_h_, pin_h);
-    initServo(this->servo_v_, pin_v);
-}
-
-class MyImu
-{
-private:
-    LSM6DS3 device_;
-    float acc_x_;
-    float acc_y_;
-    float acc_z_;
-    float gyro_x_;
-    float gyro_y_;
-    float gyro_z_;
-
-public:
-    MyImu(uint8_t bus_type, uint8_t address);
-    inline float getAccX() { return acc_x_; }
-    inline float getAccY() { return acc_y_; }
-    inline float getAccZ() { return acc_z_; }
-    inline float getGyroX() { return gyro_x_; }
-    inline float getGyroY() { return gyro_y_; }
-    inline float getGyroZ() { return gyro_z_; }
-
-    void update();
+  void update();
+  status_t begin() { return device_.begin(); }
 };
 
-MyImu::MyImu(uint8_t bus_type, uint8_t address) : device_(bus_type, address)
-{
+MyImu::MyImu(uint8_t bus_type, uint8_t address) : device_(bus_type, address) {}
+
+void MyImu::update() {
+  acc_x_ = device_.readFloatAccelX();  // g
+  acc_y_ = device_.readFloatAccelY();
+  acc_z_ = device_.readFloatAccelZ();
+  gyro_x_ = device_.readFloatGyroX();  // deg/sec
+  gyro_y_ = device_.readFloatGyroY();  // deg/sec
+  gyro_z_ = device_.readFloatGyroZ();  // deg/sec
 }
 
-void MyImu::update()
-{
-    acc_x_ = device_.readFloatAccelX(); // g
-    acc_y_ = device_.readFloatAccelY();
-    acc_z_ = device_.readFloatAccelZ();
-    gyro_x_ = device_.readFloatGyroX(); // deg/sec
-    gyro_y_ = device_.readFloatGyroY(); // deg/sec
-    gyro_z_ = device_.readFloatGyroZ(); // deg/sec
+int average(int *values, int length) {
+  int sum = 0;
+  for (int i = 0; i < length; i++) {
+    sum += values[i];
+  }
+  return sum / length;
 }
 
-int average(int *values, int length)
-{
-    int sum = 0;
-    for (int i = 0; i < length; i++)
-    {
-        sum += values[i];
-    }
-    return sum / length;
-}
-
-void pushBack(int *values, int length, int new_value)
-{
-    for (int i = length - 1; i > 0; i--)
-    {
-        values[i] = values[i - 1];
-    }
-    values[0] = new_value;
+void pushBack(int *values, int length, int new_value) {
+  for (int i = length - 1; i > 0; i--) {
+    values[i] = values[i - 1];
+  }
+  values[0] = new_value;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include "tail_util.hpp"
+#include <TaskManager.h>
 
 #include "LSM6DS3.h"
 #include "Wire.h"
+#include "tail_util.hpp"
 
 Tail tail;
 
-MyImu my_imu(I2C_MODE, 0x6A); // I2C device address 0x6A
+MyImu my_imu(I2C_MODE, 0x6A);  // I2C device address 0x6A
+int imu_freq = 10;
 
 void SweepUpdate(Servo &h, Servo &v);
 
 unsigned int wait_time = 10;
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   // build-in leds
   pinMode(LEDR, OUTPUT);
@@ -25,18 +26,28 @@ void setup()
   digitalWrite(LEDG, HIGH);
   digitalWrite(LEDB, HIGH);
 
+  Serial.begin(9600);
+  if (my_imu.begin() != 0) {
+    Serial.println("Device error");
+    digitalWrite(LEDR, LOW);
+  } else {
+    Serial.println("Device OK!");
+  }
+
   tail.attachPin(4, 5);
+
+  Tasks.add("imu_task", [] { my_imu.update(); })->startFps(100);
+  Tasks.add("tail", [] { tail.update(100); })->startFps(10);
+
   delay(3000);
 }
 
-void loop()
-{
+void loop() {
+  Tasks.update();  // automatically execute tasks
 
-  if (!tail.isSwinging())
-  {
-    tail.beginRandomSwing();
+  if (tail.isSwinging() == false) {
+    if (my_imu.accSqrt() > 2.0f) {
+      tail.beginRandomSwing();
+    }
   }
-
-  tail.update(wait_time);
-  delay(wait_time);
 }
